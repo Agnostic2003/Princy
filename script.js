a4a25c0f66d62cf7bd1ec73a5d88c025ba38ee75
Agnostic2003 <117361648+Agnostic2003@users.noreply.github.com>
2023-11-12 00:25:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const countdownDate = new Date('2023-01-01'); // Change this to your birthday
+    const timerElement = document.getElementById('timer');
+
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        timerElement.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+        if (distance < 0) {
+            timerElement.innerHTML = 'It\'s Party Time!';
+            // Call the confetti function when the countdown reaches zero
+            celebrate();
+        }
+    }
+
+    setInterval(updateCountdown, 1000);
+
+    function celebrate() {
+        const confettiContainer = document.createElement('div');
+        confettiContainer.className = 'confetti-container';
+        document.body.appendChild(confettiContainer);
+
+        for (let i = 0; i < 10; i++) {
+            const confetti = document.createElement('div');
+            confetti.className = 'confetti';
+            confetti.style.backgroundColor = getRandomColor();
+            confetti.style.left = `${Math.random() * 100}vw`;
+            confetti.style.animationDuration = `${Math.random() * 2 + 1}s`;
+            confettiContainer.appendChild(confetti);
+        }
+    }
+
+    function getRandomColor() {
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+});
